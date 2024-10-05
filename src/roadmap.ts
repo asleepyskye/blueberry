d4970dc415687cf0ba991e34287f461d4db1d09f
alyssa <alyssa@mailc.net>
2024-10-06 01:11:34 +0900

    add rules/roadmap channel messages

---
+++ src/roadmap.ts
@@ -0,0 +1,36 @@
+export default {
+  content: `
+# Not sure where to go?
+
+### Useful links
+
+PluralKit's website: https://pluralkit.me
+FAQ: https://pluralkit.me/faq/
+Web dashboard (to view/edit system information): https://dash.pluralkit.me
+
+### If you have questions about how to use the bot...
+
+You can ask any bot support questions, whatever they may be, in <#667795132971614229>.\nIf you're brand new to the bot, and haven't already read the [**Getting Started Guide**](<https://pluralkit.me/start/>), we recommend you check that out before asking any questions.
+
+### Have a suggestion for a bot feature?
+
+<#468821582794588160> is the place to post any suggestions or feature requests. If you're not sure if something has been suggested before, check out [our Notion board of suggestions](<https://developing-cuckoo-ca7.notion.site/74576534b6f9446a8ca9aa8c30c4069c>) - that's where we keep track of everything people have requested.
+
+### Is something not working right?
+
+<#635146116144431149> is the place to report it. (If you're posting an error code given by the bot, please **copy-and-paste** that error code as text instead of sending a screenshot - it makes our lives much easier!)
+
+### Have any questions, suggestions or bug reports for the dashboard?
+
+All dashboard related questions go in ⁠<#961622977458360373>. Suggestions are also logged on the [Notion board](<https://www.notion.so/Docs-Tracker-e245f44e76d74543ada0f51585d01016>).
+
+### Need help with an external **integration** to PluralKit?
+
+Many websites and/or mobile apps have been created by the community. We can't help with those here; please contact the developers of the integration for support. Alternatively, ask in <#912804952953479171>.
+
+### Find plural-related servers
+
+While this server is exclusively related to PluralKit, more plural-related community and resource servers can be found in Plural Hub: https://discord.gg/2tFRMBw
+
+  `
+}
---
+++ src/rules.ts
@@ -0,0 +1,34 @@
+export default {
+  content: "# Welcome to the PluralKit support server!",
+  embeds: [
+    {
+      title: "Server Rules",
+      description: `
+### 1️⃣ No discourse, no drama, no bigotry
+
+If you want to argue, take it out of here. People blocking you is also not an excuse to bring it into the server. Don't try to ping people that have you blocked, and if someone asks you not to interact with them, respect that.\n\n**This rule also applies to expressing intolerance towards endogenic (or otherwise non-traumagenic) systems.** You're entitled to your opinions, whatever they may be, but don't express the hateful ones here.\n\nAdditionally, *screenshots from other servers are not allowed* (outside of getting support), as they're too often posted to bring drama from other servers into this space.
+
+### 2️⃣ Don't try to intentionally trigger others
+
+This server may not have a trigger list, but that's no excuse. Spoiler common triggers or don't bring them up at all.\n\nVenting is also not allowed in this server, as it commonly brings up sensitive topics and/or breaks rule 1.
+
+### 3️⃣ Keep things "safe for work"!
+
+This applies to images; usernames (including display names, when proxying); and message content. This is a public server with minors in it, and is **absolutely not the place for adult content.**
+
+### 4️⃣ Keep off-topic talk out of the on-topic channels
+
+This is a bot support server, first and foremost - not a space to socialize. Off-topic messages in any support or development channels will be deleted.\n\nIf you need to run any bot commands in this server, please do that in <#471385416595931176>. There is an off-topic channel also, which is locked behind a role - use \`?rank Chat Access\` in the aforementioned channel to get access.
+
+### 5️⃣ No advertisements
+
+This isn't the place. Don't share or request invites to Discord servers or group DMs, or discuss creating Discord servers and/or group DMs. Don't use this server to DM advertise, either.
+`
+  },
+  {
+    description: `
+If there is an issue that requires moderator attention, use the <@&846725339412693013> role. Pinging individual moderators is fine, but do be aware that people may be away and a different staff member might be able to help you sooner.
+`
+    }
+  ]
+}
